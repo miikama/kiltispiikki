8af404fa97db793f2df5641d53cab0e9bff4a90e
Miika <makelanmiika@gmail.com>
2015-12-10 13:06:57 +0200

    added screens

--- piikki/main.py
+++ piikki/main.py
@@ -1,18 +1,27 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 
 
-class MenuWindow(BoxLayout):
+Builder.load_file('piikki.kv')
+
+
+class MenuScreen(Screen):
     pass     
   
+class BuyScreen(Screen):
+    pass
+ 
     
-
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name="menu"))
+sm.add_widget(BuyScreen(name="osto"))
 
         
 class PiikkiApp(App):
 
     def build(self):
-        return MenuWindow()
+        return sm
     
 
 if __name__ == '__main__':
